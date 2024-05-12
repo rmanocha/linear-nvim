045307b83036304f66373b89889bf237e057ab00
Rishabh Manocha <rmanocha@gmail.com>
2024-05-12 06:15:26 -0700

    add some stylua settings

--- lua/linear-nvim/init.lua
+++ lua/linear-nvim/init.lua
@@ -4,53 +4,53 @@ local key_store = require("linear-nvim.key-store")
 local utils = require("linear-nvim.utils")
 
 function M.setup()
-	M.client = linear_client:setup(key_store.fetch_api_key)
+    M.client = linear_client:setup(key_store.fetch_api_key)
 end
 
 local function show_issues_picker(issues)
-	-- Prepare entries for the picker from the issues map
-	local entries = {}
-	for display_key, data_bag in pairs(issues) do
-		table.insert(entries, {
-			value = data_bag.branch_name, -- This is what will be copied to the clipboard
-			display = display_key, -- How the entry will be displayed
-			ordinal = display_key, -- Used for sorting and searching
-			description = data_bag.description, -- Additional information that can be displayed
-		})
-	end
+    -- Prepare entries for the picker from the issues map
+    local entries = {}
+    for display_key, data_bag in pairs(issues) do
+        table.insert(entries, {
+            value = data_bag.branch_name, -- This is what will be copied to the clipboard
+            display = display_key, -- How the entry will be displayed
+            ordinal = display_key, -- Used for sorting and searching
+            description = data_bag.description, -- Additional information that can be displayed
+        })
+    end
 
-	utils.show_telescope_picker(entries, "Issues")
+    utils.show_telescope_picker(entries, "Issues")
 end
 
 local function show_create_issues_result_picker(issue)
-	local entries = {
-		{
-			value = issue.url,
-			display = "Copy Issue URL",
-			ordinal = "Copy Issue URL",
-			description = issue.url,
-		},
-		{
-			value = issue.branchName,
-			display = "Copy Branch Name",
-			ordinal = "Copy Branch Name",
-			description = issue.branchName,
-		},
-		{
-			value = issue.title,
-			display = "Copy Issue Title",
-			ordinal = "Copy Issue Title",
-			description = issue.title,
-		},
-		{
-			value = issue.identifier,
-			display = "Copy Issue Identifier",
-			ordinal = "Copy Issue Identifier",
-			description = issue.identifier,
-		},
-	}
+    local entries = {
+        {
+            value = issue.url,
+            display = "Copy Issue URL",
+            ordinal = "Copy Issue URL",
+            description = issue.url,
+        },
+        {
+            value = issue.branchName,
+            display = "Copy Branch Name",
+            ordinal = "Copy Branch Name",
+            description = issue.branchName,
+        },
+        {
+            value = issue.title,
+            display = "Copy Issue Title",
+            ordinal = "Copy Issue Title",
+            description = issue.title,
+        },
+        {
+            value = issue.identifier,
+            display = "Copy Issue Identifier",
+            ordinal = "Copy Issue Identifier",
+            description = issue.identifier,
+        },
+    }
 
-	utils.show_telescope_picker(entries, "Issue created")
+    utils.show_telescope_picker(entries, "Issue created")
 end
 
 -- create a setup command the user has to call to provide an api key to use
@@ -59,44 +59,44 @@ end
 -- i.e. a panel
 
 function M.show_user_id()
-	print(M.client:get_user_id())
+    print(M.client:get_user_id())
 end
 
 function M.show_assigned_issues()
-	local issues = M.client:get_assigned_issues()
+    local issues = M.client:get_assigned_issues()
 
-	local issue_titles = {}
-	for _, issue in ipairs(issues) do
-		local description = issue.description
-		if description == vim.NIL or description == nil then
-			description = "No description available"
-		end
-		issue_titles[issue.identifier .. " - " .. issue.title] =
-			{ branch_name = issue.branchName, description = description }
-	end
+    local issue_titles = {}
+    for _, issue in ipairs(issues) do
+        local description = issue.description
+        if description == vim.NIL or description == nil then
+            description = "No description available"
+        end
+        issue_titles[issue.identifier .. " - " .. issue.title] =
+            { branch_name = issue.branchName, description = description }
+    end
 
-	show_issues_picker(issue_titles)
+    show_issues_picker(issue_titles)
 end
 
 function M.create_issue()
-	local full_selection = utils.get_visual_selection()
-	local title, description = full_selection:match("([^\n]*)\n(.*)")
+    local full_selection = utils.get_visual_selection()
+    local title, description = full_selection:match("([^\n]*)\n(.*)")
 
-	-- If there is no newline, the whole selection is the title
-	if not title then
-		title = full_selection
-		description = ""
-	end
-	if title == "" then
-		title = vim.fn.input("Enter the title of the issue: ")
-	end
-	local issue = M.client:create_issue(title, description)
-	if issue ~= nil then
-		print("Issue created successfully!")
-		show_create_issues_result_picker(issue)
-	else
-		print("Failed to create issue")
-	end
+    -- If there is no newline, the whole selection is the title
+    if not title then
+        title = full_selection
+        description = ""
+    end
+    if title == "" then
+        title = vim.fn.input("Enter the title of the issue: ")
+    end
+    local issue = M.client:create_issue(title, description)
+    if issue ~= nil then
+        print("Issue created successfully!")
+        show_create_issues_result_picker(issue)
+    else
+        print("Failed to create issue")
+    end
 end
 
 return M
